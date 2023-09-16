9e8eb272e0fff4ad95cfc048234b743aea6a23d2
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-09-16 13:10:25 +0500

    [Built: Make action and action creator function]

--- Redux Demo/app.js
+++ Redux Demo/app.js
@@ -1,1 +1,12 @@
-console.log("We are going to learn redux");
+// console.log("We are going to learn redux");
+
+// Creating action
+
+const CAKE_ORDERED = "Cake_Ordered";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
