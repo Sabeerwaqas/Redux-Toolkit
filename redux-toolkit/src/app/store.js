b62c2e85478616c3d5ba71f315537b2a56612819
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-10-08 17:49:52 +0500

    [Learn: Learned about redux store]

---
+++ redux-toolkit/src/app/store.js
@@ -0,0 +1,10 @@
+/*
+    Redux store and redux are kind of use interchangeably
+    both stand for container for JavaScript apps. It stores
+    the whole state of the app an immutable object tree and really
+    the intended pattern for redux is just to have a single store
+    for your application multiple stores are possible but that is
+    not what is recommended by the creators of reduxt.  
+
+*/
+
