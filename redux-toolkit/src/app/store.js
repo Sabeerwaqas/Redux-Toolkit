5e6163ff18030fc6183cf30121ed4a9cff11ae3c
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-10-08 17:59:56 +0500

    [Build: Build a redux store and import it in app component]

--- redux-toolkit/src/app/store.js
+++ redux-toolkit/src/app/store.js
@@ -8,3 +8,28 @@
 
 */
 
+// To make a store, we have to import configureStore from "@reduxjs/toolkit"
+
+import { configureStore } from "@reduxjs/toolkit";
+
+/*
+
+    In the Redux Toolkit, configureStore is a function that helps simplify the
+    process of creating a Redux store with common configurations. It is part of
+    the toolkit's efforts to streamline the Redux setup and reduce boilerplate code.
+
+*/
+
+export const store = configureStore({
+  /*
+    
+
+    In the context of Redux, a reducer is a pure function that takes the current state
+    and an action as arguments and returns the new state. The primary responsibility of
+    a reducer is to specify how the application's state changes in response to different
+    actions.
+
+    */
+
+  reducer: {},
+});
